c3e80593f6591fbc7ab57a50d5d8865c7482996d
aayushtailang1911 <aayush.tailang@nitorinfotech.com>
2024-09-16 11:55:34 +0530

    removed bootstrap

--- amazon-clone/src/main.js
+++ amazon-clone/src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { BootstrapVue} from "bootstrap-vue";
 import router from "./router";
 import store from "./store"
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import { BootstrapVue3 } from 'bootstrap-vue-3';
 
-createApp(App).use(BootstrapVue).use(router).use(store).mount("#app");
+createApp(App).use(BootstrapVue3).use(router).use(store).mount("#app");
